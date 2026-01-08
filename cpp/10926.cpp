6f725d07c863b18a30dd484d89a9ffaa52e82c68
dev-cyan <zyos1@naver.com>
2026-01-08 20:16:14 +0900

    10926

---
+++ cpp/10926.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string ans;
+    cin >> ans;
+
+    cout << ans << "??!";
+
+    return 0;
+}
