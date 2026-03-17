13167420bc3efe0a4015e0552b86980ce2781295
dev-cyan <zyos1@naver.com>
2026-03-17 16:51:27 +0900

    1927

---
+++ cpp/1927.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K;
+priority_queue<int, vector<int>, greater<int>> pq;
+vector<int> result;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    for(int i=0; i<N; i++){
+        cin >> K;
+        if(K==0) {
+            if(pq.empty()) {
+                result.push_back(0);
+            }
+            else {
+                result.push_back(pq.top());
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(K);
+        }
+    }
+
+    for(int i : result){
+        cout << i << "\n";
+    }
+
+    return 0;
+}
